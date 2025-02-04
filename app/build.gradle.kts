30d4362b6d90b3a71402404531c19a322fad0834
Alvaro Rincon <alvarorinconc1@gmail.com>
2025-02-03 20:29:38 -0500

    Configuraciones iniciales.
    - Se agregan las dependencias necesarias para el proyecto.
    - Se configura el permiso de la cámara.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -42,6 +47,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // ML Kit - Barcode Scanner
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
+    // Camera
+    implementation("androidx.camera:camera-core:1.4.1")
+    implementation("androidx.camera:camera-camera2:1.4.1")
+    implementation("androidx.camera:camera-lifecycle:1.4.1")
+    implementation("androidx.camera:camera-view:1.2.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
